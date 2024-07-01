584eade10a8fe3f92bed330af501d02cb1edfff8
Umer Sikander <90478768+Umer-450@users.noreply.github.com>
2024-07-01 15:00:35 -0400

    Update exercise2-1.c

--- simlab3/exercise2-1.c
+++ simlab3/exercise2-1.c
@@ -4,3 +4,20 @@ int getValueFromArray(int *array, int len, int index) {
     // Retrieve the element from the array at the specified index
     return array[index];
 }
+
+// code produced by CGT
+
+#include <stdio.h>
+
+int getValueFromArray(int *array, int index) {
+    // Retrieve the element from the array at the specified index
+    return array[index];
+}
+
+int main() {
+    int array[] = {10, 20, 30, 40, 50};
+    int index = 2;
+    int value = getValueFromArray(array, index);
+    printf("The value at index %d is %d\n", index, value);
+    return 0;
+}
