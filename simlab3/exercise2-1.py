ce58d2906f3fb80245fa7e37de6161a57c085800
Umer Sikander <90478768+Umer-450@users.noreply.github.com>
2024-07-01 15:09:39 -0400

    Update exercise2-1.py

--- simlab3/exercise2-1.py
+++ simlab3/exercise2-1.py
@@ -1,6 +1,6 @@
 # My code
 
-def get_value_from_array(array, index):
+def retrieveArrayValue(array, index):
     # Retrieve the element from the array at the specified index
     return array[index]
 
